20329d54eec1ad6d7d8960af611eac300edcc868
Refayat Haque <rhaque@pyramidsystems.com>
2020-11-05 23:48:20 -0500

    Nov 5 2348

--- src/components/CoachItem.vue
+++ src/components/CoachItem.vue
@@ -3,11 +3,13 @@
     <h3>{{ firstName }} {{ lastName }}</h3>
     <h4>${{ rate }}/hour</h4>
     <div>
-      <span v-for="area in areas" :key="area">{{ area }}</span>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
     </div>
     <div class="actions">
-      <router-link :to="coachContactLink">Contact</router-link>
-      <router-link :to="coachDetailsLink">View Details</router-link>
+      <base-button mode="outline" link :to="coachContactLink"
+        >Contact</base-button
+      >
+      <base-button link :to="coachDetailsLink">View Details</base-button>
     </div>
   </li>
 </template>
@@ -17,13 +19,13 @@ export default {
   props: ["id", "firstName", "lastName", "rate", "areas"],
   computed: {
     coachContactLink() {
-      return `${this.$route.path}/${this.id}/contact`
+      return `${this.$route.path}/${this.id}/contact`;
       // return `/coaches/${this.id}/contact`
     },
     coachDetailsLink() {
-      return `${this.$route.path}/${this.id}`
-    }
-  }
+      return `${this.$route.path}/${this.id}`;
+    },
+  },
 };
 </script>
 
