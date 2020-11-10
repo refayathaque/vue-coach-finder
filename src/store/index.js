5d511843de347588fbfa3e317cefe5d313fad749
Refayat Haque <rhaque@pyramidsystems.com>
2020-11-09 22:58:49 -0500

    Nov 9 2258

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,20 @@
 import { createStore } from "vuex";
 
 import coachesModule from "./modules/coaches.js";
+import requestsModule from "./modules/requests.js";
 
 const store = createStore({
-  modules: { coachesModule },
+  modules: { coachesModule, requestsModule },
   state() {
     return {
-      userId: 'c3'
-    }
+      userId: "c3",
+    };
   },
   getters: {
     userId({ userId }) {
       return userId;
-    }
-  }
+    },
+  },
 });
 
 export default store;
